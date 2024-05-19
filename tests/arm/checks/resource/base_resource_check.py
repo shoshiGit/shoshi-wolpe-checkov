f71444cc4d9d3d84b7a64a6d9518521994163d24
shoshiGit <shoshiwolpe@gmail.com>
2024-05-19 20:34:08 +0300

    StorageSyncPublicAccessDisabled fix 7

--- tests/arm/checks/resource/base_resource_check.py
+++ tests/arm/checks/resource/base_resource_check.py
@@ -1,51 +1,48 @@
-from abc import abstractmethod
-from collections.abc import Iterable
-from typing import Dict, List, Any, Optional
+import pytest
 
-from checkov.common.checks.base_check import BaseCheck
-from checkov.common.models.enums import CheckResult, CheckCategories
-from checkov.common.util.var_utils import is_terraform_variable_dependent
-from checkov.terraform.checks.resource.registry import resource_registry
-from checkov.terraform.parser_functions import handle_dynamic_values
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.terraform.checks.resource.registry import resource_registry as registry
+
+class TestStaticCheck(BaseResourceCheck):
+    # for pytest not to collect this class as tests
+    __test__ = False
+
+    def __init__(self):
+        name = "Test something"
+        id = "CKV_TEST_1"
+        supported_resources = ["ckv_test"]
+        categories = [CheckCategories.CONVENTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if "check_result" in conf.keys():
+            check_result = conf["check_result"][0]
+            if check_result:
+                return CheckResult.PASSED
+
+            return CheckResult.FAILED
+
+        return CheckResult.UNKNOWN
+
+@pytest.fixture(scope="module", autouse=True)
+def remove_check():
+    yield
+    del registry.checks["ckv_test"]
 
 
-class BaseResourceCheck(BaseCheck):
-    def __init__(
-        self,
-        name: str,
-        id: str,
-        categories: "Iterable[CheckCategories]",
-        supported_resources: "Iterable[str]",
-        guideline: Optional[str] = None,
-    ) -> None:
-        super().__init__(
-            name=name,
-            id=id,
-            categories=categories,
-            supported_entities=supported_resources,
-            block_type="resource",
-            guideline=guideline,
-        )
-        self.supported_resources = supported_resources
-        resource_registry.register(self)
+@pytest.mark.parametrize(
+    "conf,expected",
+    [
+        ({"check_result": [True]}, CheckResult.PASSED),
+        ({"check_result": [False]}, CheckResult.FAILED),
+        ({"foo": ["bar"]}, CheckResult.UNKNOWN),
+        ({"count": [0], "check_result": [True]}, CheckResult.UNKNOWN),
+        ({"count": [1], "check_result": [True]}, CheckResult.PASSED),
+    ],
+    ids=["pass", "fail", "unknown", "count_zero", "count_one"],
+)
+def test_scan_entity_conf(conf, expected):
+    result = TestStaticCheck().scan_entity_conf(conf, "ckv_test")
 
-    @staticmethod
-    def _is_variable_dependant(value: Any) -> bool:
-        return is_terraform_variable_dependent(value)
-
-    def scan_entity_conf(self, conf: Dict[str, List[Any]], entity_type: str) -> CheckResult:
-        self.entity_type = entity_type
-
-        if conf.get("count") == [0]:
-            return CheckResult.UNKNOWN
-
-        handle_dynamic_values(conf)
-        return self.scan_resource_conf(conf)
-
-    @abstractmethod
-    def scan_resource_conf(self, conf: Dict[str, List[Any]]) -> CheckResult:
-        """
-        self.evaluated_keys should be set with a JSONPath of the attribute inspected.
-        If not relevant it should be set to an empty array so the previous check's value gets overridden in the report.
-        """
-        raise NotImplementedError()
+    assert result == expected
